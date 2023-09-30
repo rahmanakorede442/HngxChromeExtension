588cecb810a3615336578722b78ea12e6845392d
Abdulmujeeb Abdulrahman <72665149+rahmanakorede442@users.noreply.github.com>
2023-09-30 01:14:40 +0100

    setup completed

---
+++ database/migrations/2023_09_30_000115_create_videos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('device_id')->nullable();
+            $table->foreignUuid('user_id')->nullable();
+            $table->string('title')->nullable();
+            $table->string('url');
+            $table->string('thumb_nail')->nullable();
+            $table->string('duration')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('videos');
+    }
+};
