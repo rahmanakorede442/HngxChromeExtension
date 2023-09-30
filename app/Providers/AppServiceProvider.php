7dd306536a2909b4081e759f27a28d98a1d81809
Abdulmujeeb Abdulrahman <72665149+rahmanakorede442@users.noreply.github.com>
2023-09-30 01:40:05 +0100

    fixed: render endpoint ish

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,10 +18,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-public function boot(UrlGenerator $url)
- {
-     if (env('APP_ENV') == 'production') {
-         $url->forceScheme('https');
-     }
- }
+    public function boot(UrlGenerator $url)
+    {
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
+    }
+
 }
